d09d4f8aa4de32674813e5ad3ffd59f72ee7ffa2
Junseo Bae <ferrater1013@gmail.com>
2026-01-02 19:43:00 +0900

    feat: 미션 시나리오 실행 로직 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,19 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import racingcar.application.controller.RacingController;
+import racingcar.config.AppConfig;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        try {
+            AppConfig appConfig = new AppConfig();
+            RacingController racingController = appConfig.racingController();
+            racingController.saveCars();
+            racingController.runGame();
+            racingController.findWinners();
+        } finally {
+            Console.close();
+        }
     }
 }
