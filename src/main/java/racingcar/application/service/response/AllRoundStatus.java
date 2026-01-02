90292bcab079ad5bbd353bbb04c5cd676867f7de
Junseo Bae <ferrater1013@gmail.com>
2026-01-02 19:35:50 +0900

    feat: 응답 DTO 도구 메서드 추가

--- src/main/java/racingcar/application/service/response/AllRoundStatus.java
+++ src/main/java/racingcar/application/service/response/AllRoundStatus.java
@@ -1,8 +1,17 @@
 package racingcar.application.service.response;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public record AllRoundStatus(
         List<List<CarStatus>> roundStatuses
 ) {
+
+    public static AllRoundStatus initialStatus() {
+        return new AllRoundStatus(new ArrayList<>());
+    }
+
+    public void addRoundStatus(List<CarStatus> carStatuses) {
+        roundStatuses().add(carStatuses);
+    }
 }
--- src/main/java/racingcar/application/service/response/CarStatus.java
+++ src/main/java/racingcar/application/service/response/CarStatus.java
@@ -1,7 +1,13 @@
 package racingcar.application.service.response;
 
+import racingcar.domain.Car;
+
 public record CarStatus(
         String name,
         int position
 ) {
+
+    public static CarStatus from(Car car) {
+        return new CarStatus(car.getName(), car.getPosition());
+    }
 }
